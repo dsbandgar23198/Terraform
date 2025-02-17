36efff2221407a93b548e7238da05f39fc54d2a7
dsbandgar23198 <dsbandgar1998@gmail.com>
2025-02-17 09:01:41 +0530

    day1,2

---
+++ Day-2-Basic-Code/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+    description = "inserting ami value"
+    type = string
+    default = "ami-0ddfba243cbee3768" 
+  
+}
+
+variable "type" {
+     type = string
+     default = "t2.micro"
+
+
+}
+
+variable "key" {
+     type = string
+     default = "MYKey"
+
+
+}
