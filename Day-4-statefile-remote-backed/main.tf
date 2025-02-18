7ad0a45875226680d5aa5ba503ad04e4733cf319
dsbandgar23198 <dsbandgar1998@gmail.com>
2025-02-18 10:27:23 +0530

    day-4-dynamodb

--- Day-4-statefile-remote-backed/main.tf
+++ Day-4-statefile-remote-backed/main.tf
@@ -11,3 +11,11 @@ resource "aws_s3_bucket" "name" {
      bucket = "dnymulticloud"
      
 }
+
+#Note:When multiple developers work on the same Terraform state file (terraform.tfstate), several problems can arise. These issues can cause conflicts, resource corruption, and deployment failures.
+#Merge Conflicts in terraform.tfstate
+#If a developer runs terraform plan on an outdated state file, and another applies changes, the state will be out of sync.
+
+
+#solution is below 
+# need to implement state locking provision by using DynamoDB process 
